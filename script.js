1ea39590929ee5aaf102d03b8ba9c9ebe431e3ac
Frank Fru <chifru19@googlemail.com>
2026-06-05 22:57:39 +0200

    Add index.html and script.js to member directory

---
+++ script.js
@@ -0,0 +1,17 @@
+fetch('data.json')
+  .then(response => response.json())
+  .then(data => {
+    const tableBody = document.getElementById('tableBody');
+    const searchInput = document.getElementById('searchInput');
+
+    function render(filter = '') {
+      tableBody.innerHTML = '';
+      data.filter(m => m.name.toLowerCase().includes(filter.toLowerCase()))
+          .forEach(m => {
+            tableBody.innerHTML += `<tr><td>${m.name}</td><td>${m.role}</td><td>${m.country}</td></tr>`;
+          });
+    }
+
+    searchInput.addEventListener('input', (e) => render(e.target.value));
+    render();
+  });
